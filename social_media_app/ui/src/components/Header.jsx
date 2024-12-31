03d2a8ef8f36a9f73dac12afe6f2e4b8155f6aae
SaliniSundaran2002 <salinimotif2002@gmail.com>
2024-12-31 19:45:49 +0530

    successfully commited

---
+++ social_media_app/ui/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = ({children}) => {
+  return (
+    <div>{children}
+    <button>create</button>
+    </div>
+  )
+}
+
+export default Header
---
+++ social_media_app/ui/src/pages/Index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <>
+    <div className='justify-between p-4 bg-blue-400'>
+        <button className='bg-blue-300'>Register</button>
+        <button>Login</button>
+    </div>
+    </>
+  )
+}
+
+export default Index
